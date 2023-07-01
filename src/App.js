e3c0fa2906ff65ed6ac55934c7fe9fed60b48bda
Leela Sri <leelasric@gmail.com>
2023-07-01 18:52:45 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-unused-vars */
 
-function App() {
+import {useDebugValue, useEffect, useState} from "react";
+import './App.css'
+import SearchIcon from './search.svg'
+import MovieCard from "./MovieCard";
+
+const API_KEY_VAL = process.env.REACT_APP_API_KEY
+const API_URL = `http://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY_VAL}`
+
+
+const App = () => {
+    const [movies,setMovies] = useState([]);
+    const [searchTerm,setSearchTerm] = useState('')
+    const searchMovies = async(title) => {
+    const response = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json()
+    setMovies(data.Search);
+  }
+  useEffect(()=> {
+    searchMovies('spiderman')
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <div className="app">
+        <h1>Idleguy</h1>
 
+        <div className="search">
+          <input
+          placeholder="search for movies"
+          value = {searchTerm}
+          onChange={(e)=>setSearchTerm(e.target.value)}
+          />
+          <img src={SearchIcon}
+          alt = 'search'
+          onClick={()=> searchMovies(searchTerm)}
+          />
+        </div>
+{
+  movies?.length > 0 
+    ? (
+       <div className="container">
+         { movies.map(a => <MovieCard movie={a}/> )}
+        </div>
+        ) : (
+          <div className="empty">
+            <h2>no movies found</h2>
+            </div>
+        )
+}
+      </div>
+
+    </>
+  );
+};
 export default App;
